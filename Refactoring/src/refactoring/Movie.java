8a030162215bf2229211df0fcd06c0fc27cc9aec
Pedro Gabriel <pedro.gabriel.diniz@accenture.com>
2022-07-26 12:36:35 -0300

    Commit 10

--- Refactoring/src/refactoring/Movie.java
+++ Refactoring/src/refactoring/Movie.java
@@ -48,4 +48,17 @@ public class Movie {
 		      
 		      return thisAmount;
 	   }
+	   
+	   
+	   public int getFrequentRenterPoints(int daysRented){
+		   int frequentRenterPoints = 0;
+
+			// add frequent renter points
+			      frequentRenterPoints ++;
+			      // add bonus for a two day new release rental
+			      if ((getPriceCode() == Movie.NEW_RELEASE) &&  daysRented > 1) return 2;
+			      else return 1;	   } 
+	   
+	   
+	   
 	}
--- Refactoring/src/refactoring/Rental.java
+++ Refactoring/src/refactoring/Rental.java
@@ -26,12 +26,6 @@ public class Rental {
 	   
 	   
 	   public int getFrequentRenterPoints() { 
-		   int frequentRenterPoints = 0;
-
-		// add frequent renter points
-		      frequentRenterPoints ++;
-		      // add bonus for a two day new release rental
-		      if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) return 2;
-		      else return 1;
+		    return _movie.getFrequentRenterPoints(_daysRented);
 		   }
 	}
