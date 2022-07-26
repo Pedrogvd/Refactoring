60c76a45927707c0112375808eef5b18137daf8a
Pedro Gabriel <pedro.gabriel.diniz@accenture.com>
2022-07-26 12:55:17 -0300

    Commit 13

--- Refactoring/src/refactoring/NewReleasePrice.java
+++ Refactoring/src/refactoring/NewReleasePrice.java
@@ -3,5 +3,10 @@ package refactoring;
 public class NewReleasePrice extends Price {
 	   public int getPriceCode() {
 	       return Movie.NEW_RELEASE;
+	       
 	   }
+	   
+	   public double getCharge(int daysRented){
+		      return daysRented * 3;
+		   } 
 	}
