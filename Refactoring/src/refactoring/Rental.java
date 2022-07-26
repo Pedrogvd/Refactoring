2255751bdb9389fbd3910bd28ca7bc09f2976dbc
Pedro Gabriel <pedro.gabriel.diniz@accenture.com>
2022-07-26 12:34:12 -0300

    Commit 9

--- Refactoring/src/refactoring/Rental.java
+++ Refactoring/src/refactoring/Rental.java
@@ -20,26 +20,7 @@ public class Rental {
 	   
 	   
 	   protected double getCharge() { //protected para ser acessado em outra classe
-		      double thisAmount = 0;
-
-			//determine amounts for each line
-		      switch (getMovie().getPriceCode()) {
-		         case Movie.REGULAR:
-		            thisAmount += 2;
-		            if (getDaysRented() > 2)
-		               thisAmount += (getDaysRented() - 2) * 1.5;
-		            break;
-		         case Movie.NEW_RELEASE:
-		            thisAmount += getDaysRented() * 3;
-		            break;
-		         case Movie.CHILDRENS:
-		            thisAmount += 1.5;
-		            if (getDaysRented() > 3)
-		               thisAmount += (getDaysRented() - 3) * 1.5;
-		             break;
-		      }
-		      
-		      return thisAmount;
+		   return _movie.getCharge(_daysRented);
 		  }
 	   
 	   
